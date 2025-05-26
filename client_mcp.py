bc28db0a10fa5a58f4909694b75b025ac770d551
Luis Rubi <luisorubi@hotmail.com>
2025-05-25 18:14:06 -0600

    adding new file structure for client development lab

---
+++ client_mcp.py
@@ -0,0 +1,9 @@
+import asyncio
+from typing import Optional
+from contextlib import AsyncExitStack
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from anthropic import Anthropic
+from dotenv import load_dotenv
